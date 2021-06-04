d737c070e6de64e03c97f773719ce9e682f253d8
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-06-04 18:20:25 +0100

    Added routes and mongoose

---
+++ routes/devices.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/read', (req, res) => {
+  
+});
+
+router.get("/read/:id", (req, res) => {
+  
+});
+
+router.post("/create", (req, res) => {
+
+});
+
+router.put("/update/:id", (req, res) => {
+
+});
+
+router.delete("/delete/:id", (req, res) => {
+  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/ping', (req, res) => {
-  return res.send('pong')
-})
+var devicesRoute = require('./routes/todo');
+
+app.use("devices", devicesRoute);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
