d6f231796138c48413fdc558bc786dba1ba4ebb0
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-06-11 17:06:24 +0100

    Added fixed routes functions

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 router.get('/read', (req, res) => getAllCategory(req, res));
 
-router.get("/read/:id", (req, res) => getCategoryById);
+router.get("/read/:id", (req, res) => getCategoryById(req, res));
 
-router.post("/create", (req, res) => addNewCategory);
+router.post("/create", (req, res) => addNewCategory(req, res));
 
-router.put("/update/:id", (req, res) => updateCategory);
+router.put("/update/:id", (req, res) => updateCategory(req, res));
 
-router.delete("/delete/:id", (req, res) => deleteCategory);
+router.delete("/delete/:id", (req, res) => deleteCategory(req, res));
 
 module.exports = router;
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 router.get('/read', (req, res) => getAllTodo(req, res));
 
-router.get("/read/:id", (req, res) => getTodoById);
+router.get("/read/:id", (req, res) => getTodoById(req, res));
 
-router.post("/create", (req, res) => addNewTodo);
+router.post("/create", (req, res) => addNewTodo(req, res));
 
-router.put("/update/:id", (req, res) => updateTodo);
+router.put("/update/:id", (req, res) => updateTodo(req, res));
 
-router.delete("/delete/:id", (req, res) => deleteTodo);
+router.delete("/delete/:id", (req, res) => deleteTodo(req, res));
 
 module.exports = router;
