77c6a236022f3d8acdf9e79e584b4770cc0ebe6a
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-06-23 14:56:55 +0100

    Added linking to correct components

--- src/Components/category/Category.jsx
+++ src/Components/category/Category.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Todo from '../todo/Todo';
 
 const Category = (props) => {
     return (
-        <div className="category">
-            <Row>
-                <Col xmd="1" className="category-id">
-                    <h4>{props.id}</h4>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs="12" className="category-name">
-                    <h3>{props.name}</h3>
-                </Col>
-            </Row>
-            <Row>
-                {props.todos.map((todo, idx) => (
-                    <Todo title={todo.title} content={todo.content} id={todo.id} key={idx} />
-                ))}
-            </Row>
-        </div>
+        <Link to={"/edit/category/" + props.id} className="link-text">
+            <div className="category">
+                <Row>
+                    <Col xmd="1" className="category-id">
+                        <h4>{props.id}</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="12" className="category-name">
+                        <h3>{props.name}</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    {props.todos.map((todo, idx) => (
+                        <Todo title={todo.title} content={todo.content} id={todo._id} key={idx} />
+                    ))}
+                </Row>
+            </div>
+        </Link >
     )
 };
 
