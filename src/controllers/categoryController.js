271be23543925a04fb611120e7b254fb0a9c5e30
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-07-06 16:56:16 +0100

    Added services

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,71 +1,40 @@
-const mongoose = require("mongoose");
-const categorySchema = require("../models/categoryModel");
-
-var Category = mongoose.model("Category", categorySchema);
-
-const addNewCategory = (req, res) => {
-  let newCategory = new Category(req.body);
-  newCategory.save((err, categoryRes) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-    res.status(201).json(categoryRes);
-  });
-};
-
-const getAllCategory = (req, res) => {
-  Category.find({})
-    .exec()
-    .then((categories, err) => {
-      if (err) {
-        res.status(404).send(err);
-      }
-      res.status(200).json(categories);
-    })
-    .catch((err) => res.status(404).send(err));
-};
-
-const getCategoryById = (req, res) => {
-  Category.findById(req.params.id)
-    .exec()
-    .then((found, err) => {
-      if (err) {
-        res.status(404).send(err);
-      }
-      res.status(200).json(found);
-    })
-    .catch((err) => res.status(404).send(err));
-};
-
-const updateCategory = (req, res) => {
-  Category.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { new: true, useFindAndModify: false },
-    (err, categoryUpdated) => {
-      if (err) {
-        res.status(200).send(err);
-      }
-      res.status(200).json(categoryUpdated);
-    }
-  );
-};
-
-const deleteCategory = (req, res) => {
-  Category.deleteOne({ _id: req.params.id }, (err, deleted) => {
-    if (err) {
-      res.status(404).send(err);
-    }
-    res
-      .status(200)
-      .json({ message: "Successfully deleted category", object: deleted });
-  });
-};
-
-module.exports = {
+const {
   addNewCategory,
   getAllCategory,
   getCategoryById,
   updateCategory,
   deleteCategory,
+} = require("../services/categoryService");
+
+const postCategory = async (req, res) => {
+  let { status, response } = await addNewCategory(req);
+  res.status(status).json(response);
+};
+
+const getCategories = async (req, res) => {
+  let { status, response } = await getAllCategory();
+  res.status(status).json(response);
+};
+
+const getCategory = async (req, res) => {
+  let { status, response } = await getCategoryById(req);
+  res.status(status).json(response);
+};
+
+const putCategory = async (req, res) => {
+  let { status, response } = await updateCategory(req);
+  res.status(status).json(response);
+};
+
+const delCategory = async (req, res) => {
+  let { status, response } = await deleteCategory(req);
+  res.status(status).json(response);
+};
+
+module.exports = {
+  postCategory,
+  getCategories,
+  getCategory,
+  putCategory,
+  delCategory,
 };
