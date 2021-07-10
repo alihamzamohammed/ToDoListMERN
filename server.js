79a81f7f8b64c35feb4e8c933ad6be4cc853b9e4
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-07-10 20:50:24 +0100

    Removed testing endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
@@ -9,37 +9,33 @@ app.use(cors());
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/todolist", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-mongoose.set('debug', (collectionName, method, query, doc) => {
-    console.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
+mongoose.set("debug", (collectionName, method, query, doc) => {
+  console.log(`${collectionName}.${method}`, JSON.stringify(query), doc);
 });
 
 var db = mongoose.connection;
-db.on('error', err => {
+db.on("error", (err) => {
   console.error(`An error occured in connecting: ${err}`);
   process.exit(1);
-})
+});
 
-db.once('open', _ => {
+db.once("open", (_) => {
   console.log("Connection established to database!");
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, "build")));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  const todoRoutes = require('./src/routes/todoRoutes');
-  const categoryRoutes = require('./src/routes/categoryRoutes');
+  const todoRoutes = require("./src/routes/todoRoutes");
+  const categoryRoutes = require("./src/routes/categoryRoutes");
 
   app.use("/todo", todoRoutes);
   app.use("/category", categoryRoutes);
 
-  app.get("/testing", (req, res) => res.json({testing: "testing new"}))
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 
   app.listen(5050);
-})
-
-
+});
